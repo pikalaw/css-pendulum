085854fd0c126c446d827825e1eb0c27eff4eeb1
Lawrence Tsang <pikalaw@gmail.com>
2020-01-23 12:05:49 -0500

    Initial.

---
+++ lib/src/ouch_component.dart
@@ -0,0 +1,9 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'ouch',
+  styleUrls: ['ouch_component.css'],
+  templateUrl: 'ouch_component.html',
+)
+class OuchComponent {
+}
---
+++ web/main.dart
@@ -0,0 +1,6 @@
+import 'package:angular/angular.dart';
+import 'package:css_pendulum/app_component.template.dart' as ng;
+
+void main() {
+  runApp(ng.AppComponentNgFactory);
+}
