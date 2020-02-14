6e2395bd56db3b4a4b87a2de0b66452575bc1046
Lawrence Tsang <pikalaw@gmail.com>
2020-02-13 21:12:48 -0500

    Skeleton code for bouncing ball.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,6 +2,7 @@ import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 
 import 'src/bars_component.dart';
+import 'src/bouncing_ball_component.dart';
 import 'src/ouch_component.dart';
 
 // AngularDart info: https://angulardart.dev
@@ -11,7 +12,8 @@ import 'src/ouch_component.dart';
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [BarsComponent, OuchComponent, coreDirectives, formDirectives],
+  directives: [BarsComponent, BouncingBallComponent, OuchComponent,
+      coreDirectives, formDirectives],
   changeDetection: ChangeDetectionStrategy.OnPush,
 )
 class AppComponent {
