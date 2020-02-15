7750bc9530ab2373e28d586026d53df8b75f3a02
Lawrence Tsang <pikalaw@gmail.com>
2020-02-14 22:29:07 -0500

    Moved bar's controls to its own component.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,7 +1,7 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 
-import 'src/bars/bars_component.dart';
+import 'src/bars/bars_playground_component.dart';
 import 'src/bouncing_ball/bouncing_ball_component.dart';
 import 'src/ouch/ouch_component.dart';
 
@@ -12,16 +12,9 @@ import 'src/ouch/ouch_component.dart';
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [BarsComponent, BouncingBallComponent, OuchComponent,
+  directives: [BarsPlaygroundComponent, BouncingBallComponent, OuchComponent,
       coreDirectives, formDirectives],
   changeDetection: ChangeDetectionStrategy.OnPush,
 )
 class AppComponent {
-  static const defaultNumBars = 10;
-  int userNumBars = defaultNumBars;
-  int get numBars => userNumBars ?? 0;
-
-  var barStyle = {
-    '--bar-color': 'black',
-  };
 }
