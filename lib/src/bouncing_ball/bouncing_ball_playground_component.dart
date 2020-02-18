44a685d6415441f07a875649ba2c281702e806a3
Lawrence Tsang <pikalaw@gmail.com>
2020-02-18 12:55:40 -0500

    Improved doc on how to use bouncing-ball.

--- lib/src/bouncing_ball/bouncing_ball_playground_component.dart
+++ lib/src/bouncing_ball/bouncing_ball_playground_component.dart
@@ -4,6 +4,7 @@ import 'package:angular/angular.dart';
   selector: 'bouncing-ball-playground',
   styleUrls: ['bouncing_ball_playground_component.css'],
   templateUrl: 'bouncing_ball_playground_component.html',
+  directives: [coreDirectives],
   changeDetection: ChangeDetectionStrategy.OnPush,
 )
 class BouncingBallPlaygroundComponent {
