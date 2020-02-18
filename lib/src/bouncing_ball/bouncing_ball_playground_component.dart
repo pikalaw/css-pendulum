3bcaa90ce3d4b65592bfbcfdd17b49f52c5e1825
Lawrence Tsang <pikalaw@gmail.com>
2020-02-17 22:15:47 -0500

    Working bouncing ball with sass only.

--- lib/src/bouncing_ball/bouncing_ball_playground_component.dart
+++ lib/src/bouncing_ball/bouncing_ball_playground_component.dart
@@ -1,12 +1,9 @@
 import 'package:angular/angular.dart';
 
-import 'bouncing_ball_component.dart';
-
 @Component(
   selector: 'bouncing-ball-playground',
   styleUrls: ['bouncing_ball_playground_component.css'],
   templateUrl: 'bouncing_ball_playground_component.html',
-  directives: [BouncingBallComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 )
 class BouncingBallPlaygroundComponent {
